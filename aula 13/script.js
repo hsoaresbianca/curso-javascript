d90c62c842e0666a4a092a07347eef362717c97b
Bianca Soares <devbiancasoares@gmail.com>
2026-07-06 21:15:16 -0300

    Create script.js

---
+++ aula 13/script.js
@@ -0,0 +1,19 @@
+
+// ESTRUTURA WHILE
+var a = 1
+while (a <= 5) {
+    console.log(`While ${a}`)
+    a++
+}
+
+//ESTRUTURA DO WHILE
+var b = 1
+do {
+    console.log(`Do-While ${b}`)
+    b++
+} while (b <= 5)
+
+// ESTRUTURA FOR
+for(c = 1; c <= 5; c++){
+    console.log(`For ${c}`)
+}
