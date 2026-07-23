a8f556ab253d0af1e08c7b8fda691602687f595d
Bianca Soares <devbiancasoares@gmail.com>
2026-07-23 14:47:09 -0300

    desafio aula 17 resolvido

--- aula 17 - desafio/EX 1/script.js
+++ aula 17 - desafio/EX 1/script.js
@@ -1,37 +1,52 @@
-let vetor = []
+// array/vetor
+let vetor = [];
 
-function adicionar(){
-    let numero = window.document.getElementById("numero");
-    let n = Number(numero.value);
+// adiciona os números no select como option
+function adicionar() {
+  let numero = window.document.getElementById("numero");
+  let n = Number(numero.value);
 
-    let selnum = window.document.getElementById("selnum");
-    let addoption = window.document.createElement("option");
+  let selnum = window.document.getElementById("selnum");
+  let option = window.document.createElement("option");
 
-    if (n < 1 || n > 100) {
-        window.alert('Digite um número válido (entre 1 e 100)')
-    } else {
-        addoption.text = `Número ${n} adicionado.`
-        selnum.appendChild(addoption)
-        vetor.push(n)
-    }
+  if (n < 1 || n > 100) {
+    window.alert("Digite um número válido (entre 1 e 100)");
+  } else if (vetor.includes(n)) {
+    window.alert(
+      `O número ${n} já foi adicionado. Adicione um número diferente.`,
+    );
+  } else {
+    option.text = `Número ${n} adicionado.`;
+    selnum.appendChild(option);
+    vetor.push(n);
+  }
 }
 
-function analisar(){
-    
-    if (vetor == 0 || vetor == 1){
-        window.alert('Digite pelo menos 2 números antes de analisar.')
-    } else {
-        function soma(){
-            return 'função soma'
-        }
-        
-        function media(){
-            return 'função media'
-        }
+// soma os números do array
+function soma(vetor) {
+  let total = 0;
+  for (let i = 0; i < vetor.length; i++) {
+    total += vetor[i];
+  }
+  return total;
+}
 
-        let r = window.document.getElementById('resultado')
+//faz a média dos números do array
+function media() {
+  let total = 0;
+  for (let i = 0; i < vetor.length; i++) {
+    total += vetor[i];
+  }
+  return total / vetor.length;
+}
 
-        r.innerHTML = `<p>Ao todo, temos ${vetor.length} números cadastrados.</p><p>O maior número informado foi ${Math.max(...vetor)}.</p><p>O menor número informado foi o ${Math.min(...vetor)}.</p><p>A soma de todos os números é de ${soma()}</p><p>A média dos números informados é de ${media()}`
-    }
+// analisa os números addicionados no array e mostra os resultados na tela
+function analisar() {
+  if (vetor.length < 2) {
+    window.alert("Digite pelo menos 2 números antes de analisar.");
+  } else {
+    let r = window.document.getElementById("resultado");
 
+    r.innerHTML = `<p>Os números adicionados foram: ${vetor}.</p><p>Ao todo, temos ${vetor.length} números cadastrados.</p><p>O maior número informado foi o ${Math.max(...vetor)}.</p><p>O menor número informado foi o ${Math.min(...vetor)}.</p><p>A soma de todos os números é ${soma(vetor)}.</p><p>A média dos números informados é ${media()}.`;
+  }
 }
